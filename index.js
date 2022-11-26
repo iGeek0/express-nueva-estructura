39149af456284691a52788da576e8280a7bf04e7
iGeek <jesus.cardenas.d@gmail.com>
2022-11-26 12:06:57 -0600

    funcional mongose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const rutasUsuarios = require('./routes/usuarios.routes');
+const { dbConnection } = require('./database/config');
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT;
@@ -14,8 +15,9 @@ app.get("/", function (req, res) {
     res.send("API v1.0");
 });
 
-(()=>{
+(async()=>{
     // Funcion que se ejecuta al entrar en el archivo index.js
+    await dbConnection();
     const rutaBase = '/api/v1';
     app.use(rutaBase, rutasUsuarios);
 })();
